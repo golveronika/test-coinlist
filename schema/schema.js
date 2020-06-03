12fdc2521b457731557410fc56841342a6a354cc
Вероника Голованова <veronika@MacBook-Pro-Veronika-2.local>
2020-06-03 11:17:18 +0200

    use fetch instead coingecko-api

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,9 @@
-var  graphql  = require('graphql')
-const CoinGecko = require('coingecko-api')
+const graphql  = require('graphql')
+const fetch = require('node-fetch')
+// const CoinGecko = require('coingecko-api')
 
 // Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
+// const CoinGeckoClient = new CoinGecko();
 
 const  { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList } = graphql;
 
@@ -22,16 +23,18 @@ const Query = new GraphQLObjectType ({
         getCoins: {
             type: new GraphQLList(CoinType),
             async resolve(parent, args) {
-                const { data } = await CoinGeckoClient.coins.list();
-                return data;
+                // const { data } = await CoinGeckoClient.coins.list()
+                // return data
+                const response = await fetch('https://api.coingecko.com/api/v3/coins/list')
+                return response.json()
             }
         },
         getCoin: {
             type: CoinType,
             args: { id: { type: GraphQLID }},
             async resolve(parent, args) {
-                const { data } = await CoinGeckoClient.coins.list();
-                return data.find(coin => coin.id === args.id);
+                const response = await fetch(`https://api.coingecko.com/api/v3/coins/${args.id}`)
+                return response.json()
             }
         },
     }
